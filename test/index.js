d133bbcf322f5f2dc4407e989db4d671ef5fc287
严俊东 <1172482914@qq.com>
2019-09-21 15:24:51 +0800

    测试

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const find = require('../index')
+
+console.log(__dirname)
+console.log(find(__dirname))
+console.log(JSON.stringify(find(__dirname)))
+console.log('#########')
+console.log(__filename)
+console.log(find(__filename))
